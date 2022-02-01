c5cf392c2709494a89e4bef3eae25af5345c77f2
Felipe Figueiredo <philsf79@gmail.com>
2022-01-31 22:40:35 -0300

    drop Rao correction in favor of Chisq design adjustment

--- scripts/modeling.R
+++ scripts/modeling.R
@@ -2,11 +2,10 @@
 library(broom)
 
 # raw estimate ------------------------------------------------------------
-dv_chi <- svychisq(~dv + dsex, svy) %>% tidy
+dv_chi <- svychisq(~dv + dsex, svy, statistic = "Chisq") %>% tidy
 dv_chi_p <- dv_chi %>% pull(p.value)
 
-iv_chi <- svychisq(~iv + dsex, svy) %>% tidy
+iv_chi <- svychisq(~iv + dsex, svy, statistic = "Chisq") %>% tidy
 iv_chi_p <- dv_chi %>% pull(p.value)
 
 # adjusted ----------------------------------------------------------------
-
