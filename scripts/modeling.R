7cf241d97971213ced236b7c206143ac995a73a2
Felipe Figueiredo <philsf79@gmail.com>
2022-01-31 22:24:07 -0300

    p-values from dv and iv

--- scripts/modeling.R
+++ scripts/modeling.R
@@ -1,7 +1,12 @@
 # setup -------------------------------------------------------------------
+library(broom)
 
 # raw estimate ------------------------------------------------------------
+dv_chi <- svychisq(~dv + dsex, svy) %>% tidy
+dv_chi_p <- dv_chi %>% pull(p.value)
 
+iv_chi <- svychisq(~iv + dsex, svy) %>% tidy
+iv_chi_p <- dv_chi %>% pull(p.value)
 
 # adjusted ----------------------------------------------------------------
 
