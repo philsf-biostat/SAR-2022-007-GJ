f0b43671bb17f72fb8260f72a295290ddf86dab2
Felipe Figueiredo <philsf79@gmail.com>
2022-02-01 04:11:14 -0300

    chi-square p-values for sex

--- scripts/modeling.R
+++ scripts/modeling.R
@@ -6,6 +6,6 @@ dv_chi <- svychisq(~dv + dsex, svy, statistic = "Chisq") %>% tidy
 dv_chi_p <- dv_chi %>% pull(p.value)
 
 iv_chi <- svychisq(~iv + dsex, svy, statistic = "Chisq") %>% tidy
-iv_chi_p <- dv_chi %>% pull(p.value)
+iv_chi_p <- iv_chi %>% pull(p.value)
 
 # adjusted ----------------------------------------------------------------
